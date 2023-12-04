8b4e69f96b69362660f2655f6af67c24d7b72d0e
Trikzy7 <theoplebani2002@icloud.com>
2023-12-04 15:01:41 +0100

    Map fichier text

--- src/WordCountMapReduce.java
+++ src/WordCountMapReduce.java
@@ -2,25 +2,22 @@ import java.util.Map;
 
 public class WordCountMapReduce {
     public static void main(String[] args) {
-        // Exemple de documents
-        String document1 = "Hello world, hello Java";
-        String document2 = "Java is a programming language";
-        String document3 = "World of Java programming";
+        // Nom de fichier par défaut
+        String inputFileName = "src/The_Fellowship_Of_The_Ring.txt";
 
         // Phase Map
-        Map<String, Integer> map1 = WordCountMapper.map(document1);
-        Map<String, Integer> map2 = WordCountMapper.map(document2);
-        Map<String, Integer> map3 = WordCountMapper.map(document3);
+        Map<String, Integer> map = WordCountMapper.map(inputFileName);
 
-        // Phase Shuffle and Sort : Non nécessaire dans cet exemple simple
-
-        // Phase Reduce
-        Map<String, Integer> finalWordCount = WordCountReducer.reduce(map1, WordCountReducer.reduce(map2, map3));
+        // Phase Reduce (pas nécessaire dans cet exemple, car nous n'avons qu'un seul map)
+        Map<String, Integer> finalWordCount = map;
 
         // Affichage du résultat final
         System.out.println("Word Count:");
         for (Map.Entry<String, Integer> entry : finalWordCount.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+
+        System.out.println("Répertoire de travail : " + System.getProperty("user.dir"));
+
     }
 }
--- src/WordCountMapper.java
+++ src/WordCountMapper.java
@@ -1,15 +1,25 @@
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordCountMapper {
-    public static Map<String, Integer> map(String document) {
+    public static Map<String, Integer> map(String fileName) {
         Map<String, Integer> wordCountMap = new HashMap<>();
-        StringTokenizer tokenizer = new StringTokenizer(document);
 
-        while (tokenizer.hasMoreTokens()) {
-            String word = tokenizer.nextToken().toLowerCase();
-            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                while (tokenizer.hasMoreTokens()) {
+                    String word = tokenizer.nextToken().toLowerCase();
+                    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return wordCountMap;
