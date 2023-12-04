444d6fe45f189de4adc05f49f9fd09900b3491b2
Trikzy7 <theoplebani2002@icloud.com>
2023-12-04 15:23:15 +0100

    Separation txt in multiple parts and map each one and reduce all of them in one

--- src/WordCountMapper.java
+++ src/WordCountMapper.java
@@ -1,27 +1,75 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class WordCountMapper {
-    public static Map<String, Integer> map(String fileName) {
+    public static List<Map<String, Integer>> map(String fileName, int numMappers) {
+        List<Map<String, Integer>> mapResults = new ArrayList<>();
+
+        // Lecture du contenu du fichier
+        String fileContent = readFile(fileName);
+
+        // Division du contenu en morceaux
+        List<String> fileParts = splitContent(fileContent, numMappers);
+
+        // Phase Map
+        for (String part : fileParts) {
+            mapResults.add(mapSinglePart(part));
+        }
+
+        return mapResults;
+    }
+
+    private static Map<String, Integer> mapSinglePart(String content) {
         Map<String, Integer> wordCountMap = new HashMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                StringTokenizer tokenizer = new StringTokenizer(line);
-                while (tokenizer.hasMoreTokens()) {
-                    String word = tokenizer.nextToken().toLowerCase();
-                    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        StringTokenizer tokenizer = new StringTokenizer(content);
+        while (tokenizer.hasMoreTokens()) {
+            String word = tokenizer.nextToken().toLowerCase();
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
         }
 
         return wordCountMap;
+    }
+
+    private static String readFile(String fileName) {
+        // Lisez le contenu du fichier et retournez-le sous forme de chaîne
+        // Vous pouvez utiliser différentes méthodes pour cela, par exemple FileReader, Files.readAllBytes, etc.
+        // Voici une version simple pour les besoins de l'exemple :
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            return content.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private static List<String> splitContent(String content, int numParts) {
+        List<String> fileParts = new ArrayList<>();
+        int contentLength = content.length();
+        int partSize = contentLength / numParts;
+
+        for (int i = 0; i < numParts; i++) {
+            int start = i * partSize;
+            int end = (i + 1) * partSize;
+            if (i == numParts - 1) {
+                end = contentLength;
+            }
+
+            String partContent = content.substring(start, end);
+            fileParts.add(partContent);
+        }
+
+        return fileParts;
     }
 }
--- src/WordCountReducer.java
+++ src/WordCountReducer.java
@@ -1,13 +1,19 @@
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordCountReducer {
-    public static Map<String, Integer> reduce(Map<String, Integer> map1, Map<String, Integer> map2) {
-        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
-            String word = entry.getKey();
-            int count = entry.getValue();
-            map1.put(word, map1.getOrDefault(word, 0) + count);
+    public static Map<String, Integer> reduce(List<Map<String, Integer>> mapResults) {
+        // Implémentez la logique de réduction ici
+        // Dans cet exemple, nous combinons simplement les résultats de tous les Mappers
+        Map<String, Integer> finalResult = new HashMap<>();
+        for (Map<String, Integer> resultMap : mapResults) {
+            for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
+                String word = entry.getKey();
+                int count = entry.getValue();
+                finalResult.put(word, finalResult.getOrDefault(word, 0) + count);
+            }
         }
-
-        return map1;
+        return finalResult;
     }
 }
