0dc907043bf18b3ec2df55d98331b0e9e1392fa2
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-12-04 15:32:45 +0100

    ponctuations

--- src/WordCountMapper.java
+++ src/WordCountMapper.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -15,6 +16,25 @@ public class WordCountMapper {
                 StringTokenizer tokenizer = new StringTokenizer(line);
                 while (tokenizer.hasMoreTokens()) {
                     String word = tokenizer.nextToken().toLowerCase();
+                    String derniereLettre = String.valueOf(word.charAt(word.length() - 1));
+                    ArrayList<String> ponctuations = new ArrayList<String>();
+                    ponctuations.add(".");
+                    ponctuations.add(",");
+                    ponctuations.add(";");
+                    ponctuations.add("!");
+                    ponctuations.add("?");
+                    ponctuations.add("`");
+                    ponctuations.add("_");
+                    ponctuations.add("-");
+                    ponctuations.add("\"");
+                    ponctuations.add(":");
+                    ponctuations.add("'");
+                    while ((ponctuations.contains(derniereLettre))&&(word.length()!=1)) {
+                        System.out.println(word);
+                        // Supprimer la dernière lettre
+                        word = word.substring(0, word.length() - 1);
+                        derniereLettre = String.valueOf(word.charAt(word.length() - 1));
+                    }
                     wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
                 }
             }
